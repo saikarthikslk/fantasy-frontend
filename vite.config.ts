0370612b26636432b9321f4e4fad734e5c12ea11
sai karthik <72986369+saikarthikslk@users.noreply.github.com>
2026-04-03 19:41:42 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
    allowedHosts: ['*','sustainability-ellis-caps-learn.trycloudflare.com'],
     proxy: {
       '/api': {
-        target: 'https://23ae-123-201-171-91.ngrok-free.app',
+        target: 'https://c8fd-123-201-171-91.ngrok-free.app',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
